2e19d97ad6ea5b7e5057f65d12e47e614d212214
lilvoss <80283723+lilvoss@users.noreply.github.com>
2021-05-06 05:45:04 +0000

    Delete fonctions.h

--- fonctions.h
+++
@@ -1,8 +0,0 @@
-#ifndef FONCTIONS_H_
-#define FONCTIONS_H_
-
-void afficher(SDL_Surface *ecran,char *chaine);
-void correcte(SDL_Surface *ecran,Mix_Chunk *corr);
-void incorrecte(SDL_Surface *ecran,Mix_Chunk *not );
-void resultat(int ligneVoulu,int jew, SDL_Surface *ecran,Mix_Chunk *corr,Mix_Chunk *not);
-#endif 
